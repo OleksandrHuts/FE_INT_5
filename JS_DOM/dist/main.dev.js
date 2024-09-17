2069b63469cbcb168749758d5d489fbbcbab9505
Oleksandr Huts <oleksandr.huts@valtech.com>
2024-09-17 22:11:00 +0300

    events

--- JS_DOM/dist/main.dev.js
+++ JS_DOM/dist/main.dev.js
@@ -3,19 +3,13 @@
 //to access an element:
 // getElementsByTagName
 // getElementById
+// querySelector
+// querySelectorAll
 //to get text from an element:
 //textContent
 //innerHTML
 //to change styling
 //.style
-function showMessage() {
-  var modal = document.querySelector('.modal');
-  var button = document.querySelector('#show_button');
-  console.log(button.setAttribute('id', 'new_id_val'));
-  modal.classList.contains('visible') ? button.textContent = 'show modal' : button.textContent = 'close modal';
-  modal.classList.toggle('visible');
-}
-
 var usersArr = [{
   id: 2,
   userName: 'Jane',
@@ -43,6 +37,17 @@ var usersArr = [{
   role: 'admin'
 }];
 
+function deleteUser(event) {
+  var id = event.target.dataset.id; // 11
+
+  var targetIndex = usersArr.findIndex(function (el) {
+    return el.id === +id;
+  }); // 5
+
+  usersArr.splice(targetIndex, 1);
+  renderGrid();
+}
+
 function createGridElement(user) {
   var grid = document.querySelector('.grid');
   var div = document.createElement('div');
@@ -54,13 +59,57 @@ function createGridElement(user) {
   var userRole = document.createElement('div');
   userRole.classList.add('user-role');
   userRole.textContent = user.role;
+  var deleteButton = document.createElement('button');
+  deleteButton.classList.add('btn');
+  deleteButton.classList.add('btn-danger');
+  deleteButton.textContent = 'Delete';
+  deleteButton.setAttribute('data-id', user.id);
+  deleteButton.addEventListener('click', deleteUser); // deleteButton.addEventListener('click', (event) => {
+  //     event.stopPropagation();
+  //     console.log('Button has been clicked')
+  // })
+
   div.appendChild(userName);
   div.appendChild(userRole);
+  div.appendChild(deleteButton);
   grid.appendChild(div);
 }
 
 function renderGrid() {
+  document.querySelector('.grid').innerHTML = '';
   usersArr.forEach(function (user) {
     createGridElement(user);
   });
-}
+} // document.addEventListener('click', event => {
+//     console.log('document has been clicked');
+// })
+// document.addEventListener('contextmenu', (event) => {
+//     event.preventDefault();
+//     console.log(event);
+//     const modal = document.querySelector('.modal');
+//     modal.classList.add('visible');
+//     modal.style.top = event.clientY + 'px';
+//     modal.style.left = event.clientX + 'px';
+// })
+// let counter = 0;
+// const buttonLabel = 'clicked: '
+// const button = document.querySelector('#click_button');
+// button.addEventListener('mousedown', (event) => {
+//     console.log(event.target);
+//     button.textContent = buttonLabel + ' ' + counter
+//     counter++
+// });
+// document.getElementById('anchor').addEventListener('click', event => {
+//     event.preventDefault();
+//     setTimeout(() => {
+//         window.location.href = event.target.dataset.href;
+//     }, 3000);
+// })
+
+
+document.addEventListener('keydown', function (event) {
+  // event.preventDefault()
+  if (event.code === 'KeyE' && event.ctrlKey) {
+    console.log('ctrl + E');
+  }
+});
--- JS_DOM/main.js
+++ JS_DOM/main.js
@@ -1,6 +1,8 @@
 //to access an element:
 // getElementsByTagName
 // getElementById
+// querySelector
+// querySelectorAll
 
 //to get text from an element:
 //textContent
@@ -8,19 +10,6 @@
 
 //to change styling
 //.style
-
-function showMessage() {
-    const modal = document.querySelector('.modal');
-    const button = document.querySelector('#show_button');
-
-    console.log(button.setAttribute('id','new_id_val'));
-    
-
-    modal.classList.contains('visible') ? button.textContent = 'show modal' : button.textContent = 'close modal';
-
-    modal.classList.toggle('visible');
-    
-}
 
 const usersArr = [
     {
@@ -55,6 +44,14 @@ const usersArr = [
     }
 ];
 
+function deleteUser(event) {
+    const id = event.target.dataset.id; // 11
+    const targetIndex = usersArr.findIndex(el => el.id === +id); // 5
+
+    usersArr.splice(targetIndex, 1);
+    renderGrid();
+}
+
 
 function createGridElement(user) {
     const grid = document.querySelector('.grid');
@@ -70,16 +67,71 @@ function createGridElement(user) {
     userRole.classList.add('user-role');
     userRole.textContent = user.role;
 
-    
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('btn');
+    deleteButton.classList.add('btn-danger');
+    deleteButton.textContent = 'Delete';
+    deleteButton.setAttribute('data-id', user.id);
+    deleteButton.addEventListener('click', deleteUser)
+    // deleteButton.addEventListener('click', (event) => {
+    //     event.stopPropagation();
+    //     console.log('Button has been clicked')
+    // })
 
     div.appendChild(userName);
     div.appendChild(userRole);
+    div.appendChild(deleteButton);
     
     grid.appendChild(div);
 }
 
 function renderGrid() {
+    document.querySelector('.grid').innerHTML = '';
+    
     usersArr.forEach(user => {
         createGridElement(user)
     })
 }
+
+// document.addEventListener('click', event => {
+//     console.log('document has been clicked');
+// })
+
+// document.addEventListener('contextmenu', (event) => {
+//     event.preventDefault();
+//     console.log(event);
+    
+//     const modal = document.querySelector('.modal');
+//     modal.classList.add('visible');
+//     modal.style.top = event.clientY + 'px';
+//     modal.style.left = event.clientX + 'px';
+// })
+
+// let counter = 0;
+// const buttonLabel = 'clicked: '
+// const button = document.querySelector('#click_button');
+
+// button.addEventListener('mousedown', (event) => {
+
+//     console.log(event.target);
+//     button.textContent = buttonLabel + ' ' + counter
+//     counter++
+// });
+
+// document.getElementById('anchor').addEventListener('click', event => {
+//     event.preventDefault();
+
+//     setTimeout(() => {
+//         window.location.href = event.target.dataset.href;
+//     }, 3000);
+// })
+
+document.addEventListener('keydown', event => {
+    // event.preventDefault()
+    
+
+    if(event.code === 'KeyE' && event.ctrlKey) {
+        console.log('ctrl + E');
+    }
+    
+})
